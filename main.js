95925125d9e82bbaa66033a46febed5f9e6d5851
Leonardo <reisleo619@gmail.com>
2024-05-10 15:32:39 -0300

    Finalização parte 1

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 
+
+$('header img').click(function(){
+    $('form').slideDown()
+})
+
+$('#cancel').click(function(){
+    $('form').slideUp();
+})
+$('form').on('submit', function(e){
+    e.preventDefault();
+    const enrderecoNovaImagem = $('#endereço-imagem-nova').val()
+    const novoItem = $('<li></li>')
+    $(`<img src="${enrderecoNovaImagem}" />`).appendTo(novoItem)
+    $(
+        `<div class="link" />
+            <a href="${enrderecoNovaImagem}" target=_"blank" title="Ver imagem em tela cheia">
+                Ver imagem em tela cheia
+            </a>
+        </div>    
+    `).appendTo(novoItem)
+
+    $(novoItem).appendTo('ul')
+    console.log(novoItem)
+})
